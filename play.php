522a871a08a80f5d00cea96c4b8db00f56cbe108
Dakota Knutson <dakota@ddev.im>
2014-09-05 00:20:23 +0400

    Added playground (play.php) for testing

---
+++ play.php
@@ -0,0 +1,18 @@
+<html><head><title>mongoAdmin Playground</title></head><body><h1>mongoAadmin Playground</h1><hr>
+<?php
+
+// Confirm that the mongo drivers ar loaded.
+echo extension_loaded('mongo.so');
+
+echo "<pre>";
+print_r(get_loaded_extensions());
+echo "</pre>";
+
+//$m = MongoClient();
+
+//var_dump($m);
+
+
+//phpinfo();
+?>
+</body><html>
